b526372d840de03735b435c0b0324060e41be356
Quang Hoa <doan.quang.hoa@sun-asterisk.com>
2020-02-13 09:42:07 +0700

    a

--- application/views/project_detail.php
+++ application/views/project_detail.php
@@ -28,7 +28,7 @@
 					<div class="fb-like"data-href="<?php echo base_url().'chi-tiet-du-an/'.$project->id; ?>" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
 				</div>
 			</div>
-			<div class="fb-comments col-md-12" data-href="<?php echo base_url().'chi-tiet-du-an/'.$project->id; ?>" data-width="" data-numposts="5"></div>
+			<div class="fb-comments col-md-12" data-href="<?php echo base_url().'chi-tiet-du-an/'.$project->id; ?>" data-width="100%" data-numposts="5"></div>
 
 		</div>
 	</div>
