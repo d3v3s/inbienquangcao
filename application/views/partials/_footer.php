248573b864a6264bf7c3d146b0930398c7373e98
Quang Hoa <doan.quang.hoa@sun-asterisk.com>
2020-02-13 10:30:19 +0700

    a

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!-- Start Footer Section -->
 	<footer id="footer">
-<!--		<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>-->
-<!--		<script src="https://sp.zalo.me/plugins/sdk.js"></script>-->
+		<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+		<script src="https://sp.zalo.me/plugins/sdk.js"></script>
 		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=564841624086506&autoLogAppEvents=1"></script>
 		<div class="footer-widget">
 			<div class="container">
