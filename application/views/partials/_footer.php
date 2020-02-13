e9103ed8baf2086bc4345f7a82c5688bd0c9c024
Quang Hoa <doan.quang.hoa@sun-asterisk.com>
2020-02-13 10:21:28 +0700

    a

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -4,29 +4,6 @@
 <!--		<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>-->
 <!--		<script src="https://sp.zalo.me/plugins/sdk.js"></script>-->
 		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=564841624086506&autoLogAppEvents=1"></script>
-		<div id="fb-root"></div>
-		<script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                    xfbml            : true,
-                    version          : 'v6.0'
-                });
-            };
-
-            (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-
-		<!-- Your customer chat code m.me/106181070965479-->
-		<div class="fb-customerchat"
-			 attribution=setup_tool
-			 page_id="106181070965479">
-		</div>
-
 		<div class="footer-widget">
 			<div class="container">
 				<div class="row">
--- application/views/partials/_header.php
+++ application/views/partials/_header.php
@@ -100,6 +100,28 @@ if ($language->id == $site_lang->id):?>
     </script>
 </head>
 <body>
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v6.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code m.me/106181070965479-->
+<div class="fb-customerchat"
+	 attribution=setup_tool
+	 page_id="106181070965479">
+</div>
 <div id="page" class="page-wrap">
 	<!-- header -->
 	<!-- Page Loader -->
