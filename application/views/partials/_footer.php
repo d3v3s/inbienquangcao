d2fc0f9529305478afb283e173b743a45e57d23a
Quang Hoa <doan.quang.hoa@sun-asterisk.com>
2020-02-13 10:06:42 +0700

    a

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -5,7 +5,7 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=564841624086506&autoLogAppEvents=1";
+        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
