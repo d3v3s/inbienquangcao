b19e1b758132ce188b3c35ff01299ced83665ec9
Quang Hoa <doan.quang.hoa@sun-asterisk.com>
2020-02-13 09:20:15 +0700

    a

--- application/views/auth/admin_login.php
+++ application/views/auth/admin_login.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo html_escape($title); ?> - <?php echo trans("admin"); ?>&nbsp;<?php echo html_escape($settings->site_title); ?></title>
+    <title>Trang Quản Trị</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<?php if (empty($general_settings->favicon_path)): ?>
