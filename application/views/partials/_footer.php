1cf921bbf61955837762f6b08ec6a0911e308224
Quang Hoa <hoa9x3@gmail.com>
2020-02-15 01:02:12 +0700

    a

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -1,30 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div id="fb-root"></div>
-<script>
-	window.fbAsyncInit = function() {
-		FB.init({
-			xfbml            : true,
-			version          : 'v6.0'
-		});
-	};
 
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-	 attribution=setup_tool
-	 page_id="106181070965479"
-	 theme_color="#0084ff"
-	 logged_in_greeting="Xin chào, bạn cần tư vấn về mẫu biển quảng cáo nào?"
-	 logged_out_greeting="Xin chào, bạn cần tư vấn về mẫu biển quảng cáo nào?">
-</div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=564841624086506&autoLogAppEvents=1"></script>
 <footer id="footer">
 	<div class="footer-widget">
 		<div class="container">
